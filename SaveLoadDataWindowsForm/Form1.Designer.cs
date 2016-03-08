fbdd2479d7f370079e7d514a99fffd7d04b4b0b2
AKM Rezaul Alam <arezaulalam@gmail.com>
2016-03-08 12:44:15 +0600

    Dynamic Data State Save and Load

--- SaveLoadDataWindowsForm/Form1.Designer.cs
+++ SaveLoadDataWindowsForm/Form1.Designer.cs
@@ -28,12 +28,157 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.rowNameTextBox = new System.Windows.Forms.TextBox();
+            this.addButton = new System.Windows.Forms.Button();
+            this.saveButton = new System.Windows.Forms.Button();
+            this.LoadButton = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.mainDataGridView = new System.Windows.Forms.DataGridView();
+            this.checkBox = new System.Windows.Forms.DataGridViewCheckBoxColumn();
+            this.rowName = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.indLocation = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.iBrowse = new System.Windows.Forms.DataGridViewButtonColumn();
+            this.depLocation = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dBrowse = new System.Windows.Forms.DataGridViewButtonColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.mainDataGridView)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // rowNameTextBox
+            // 
+            this.rowNameTextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.rowNameTextBox.Location = new System.Drawing.Point(39, 41);
+            this.rowNameTextBox.Name = "rowNameTextBox";
+            this.rowNameTextBox.Size = new System.Drawing.Size(162, 34);
+            this.rowNameTextBox.TabIndex = 0;
+            // 
+            // addButton
+            // 
+            this.addButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.addButton.Location = new System.Drawing.Point(238, 41);
+            this.addButton.Name = "addButton";
+            this.addButton.Size = new System.Drawing.Size(134, 34);
+            this.addButton.TabIndex = 1;
+            this.addButton.Text = "Add";
+            this.addButton.UseVisualStyleBackColor = true;
+            this.addButton.Click += new System.EventHandler(this.addButton_Click);
+            // 
+            // saveButton
+            // 
+            this.saveButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.saveButton.Location = new System.Drawing.Point(701, 41);
+            this.saveButton.Name = "saveButton";
+            this.saveButton.Size = new System.Drawing.Size(115, 34);
+            this.saveButton.TabIndex = 2;
+            this.saveButton.Text = "Save";
+            this.saveButton.UseVisualStyleBackColor = true;
+            this.saveButton.Click += new System.EventHandler(this.saveButton_Click);
+            // 
+            // LoadButton
+            // 
+            this.LoadButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LoadButton.Location = new System.Drawing.Point(836, 41);
+            this.LoadButton.Name = "LoadButton";
+            this.LoadButton.Size = new System.Drawing.Size(122, 34);
+            this.LoadButton.TabIndex = 3;
+            this.LoadButton.Text = "Load";
+            this.LoadButton.UseVisualStyleBackColor = true;
+            this.LoadButton.Click += new System.EventHandler(this.LoadButton_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(416, 41);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(246, 29);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Save and Load Data";
+            // 
+            // mainDataGridView
+            // 
+            this.mainDataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.mainDataGridView.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.checkBox,
+            this.rowName,
+            this.indLocation,
+            this.iBrowse,
+            this.depLocation,
+            this.dBrowse});
+            this.mainDataGridView.Location = new System.Drawing.Point(39, 108);
+            this.mainDataGridView.Name = "mainDataGridView";
+            this.mainDataGridView.RowTemplate.Height = 24;
+            this.mainDataGridView.Size = new System.Drawing.Size(919, 449);
+            this.mainDataGridView.TabIndex = 5;
+            this.mainDataGridView.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.mainDataGridView_CellContentClick);
+            // 
+            // checkBox
+            // 
+            this.checkBox.HeaderText = "Check";
+            this.checkBox.Name = "checkBox";
+            this.checkBox.Width = 30;
+            // 
+            // rowName
+            // 
+            this.rowName.HeaderText = "rowName";
+            this.rowName.Name = "rowName";
+            // 
+            // indLocation
+            // 
+            this.indLocation.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.indLocation.HeaderText = "iLocation";
+            this.indLocation.Name = "indLocation";
+            // 
+            // iBrowse
+            // 
+            this.iBrowse.HeaderText = "iBrowse";
+            this.iBrowse.Name = "iBrowse";
+            // 
+            // depLocation
+            // 
+            this.depLocation.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.depLocation.HeaderText = "dLocation";
+            this.depLocation.MinimumWidth = 25;
+            this.depLocation.Name = "depLocation";
+            // 
+            // dBrowse
+            // 
+            this.dBrowse.HeaderText = "dBrowse";
+            this.dBrowse.Name = "dBrowse";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(998, 589);
+            this.Controls.Add(this.mainDataGridView);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.LoadButton);
+            this.Controls.Add(this.saveButton);
+            this.Controls.Add(this.addButton);
+            this.Controls.Add(this.rowNameTextBox);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load_1);
+            ((System.ComponentModel.ISupportInitialize)(this.mainDataGridView)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox rowNameTextBox;
+        private System.Windows.Forms.Button addButton;
+        private System.Windows.Forms.Button saveButton;
+        private System.Windows.Forms.Button LoadButton;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DataGridView mainDataGridView;
+        private System.Windows.Forms.DataGridViewCheckBoxColumn checkBox;
+        private System.Windows.Forms.DataGridViewTextBoxColumn rowName;
+        private System.Windows.Forms.DataGridViewTextBoxColumn indLocation;
+        private System.Windows.Forms.DataGridViewButtonColumn iBrowse;
+        private System.Windows.Forms.DataGridViewTextBoxColumn depLocation;
+        private System.Windows.Forms.DataGridViewButtonColumn dBrowse;
     }
 }
 
--- SaveLoadDataWindowsForm/Form1.cs
+++ SaveLoadDataWindowsForm/Form1.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -15,6 +16,198 @@ namespace SaveLoadDataWindowsForm
         public Form1()
         {
             InitializeComponent();
+        }
+
+       
+
+        private void addButton_Click(object sender, EventArgs e)
+        {
+            if (rowNameTextBox.Text == "")
+            {
+
+            }
+            else
+            {
+                mainDataGridView.Rows.Add(1);
+                mainDataGridView.Rows[(mainDataGridView.Rows.Count - 1)].Cells[0].Value = false;
+                mainDataGridView.Rows[(mainDataGridView.Rows.Count - 1)].Cells[1].Value = rowNameTextBox.Text;
+                mainDataGridView.Rows[(mainDataGridView.Rows.Count - 1)].Cells[2].Value = "";
+                mainDataGridView.Rows[(mainDataGridView.Rows.Count - 1)].Cells[3].Value = "Browse";
+                mainDataGridView.Rows[(mainDataGridView.Rows.Count - 1)].Cells[4].Value = "";
+                mainDataGridView.Rows[(mainDataGridView.Rows.Count - 1)].Cells[5].Value = "Browse";  
+            }
+            
+           
+        }
+
+        private void Form1_Load_1(object sender, EventArgs e)
+        {
+
+
+            string line;
+            mainDataGridView.AllowUserToAddRows = false;
+
+            StreamReader fileN = new StreamReader("..\\save.txt");
+
+            while ((line = fileN.ReadLine()) != null)
+            {
+                mainDataGridView.Rows.Add(1);
+                mainDataGridView.Rows[(mainDataGridView.Rows.Count - 1)].Cells[0].Value = false;
+                mainDataGridView.Rows[(mainDataGridView.Rows.Count - 1)].Cells[1].Value = "";
+                mainDataGridView.Rows[(mainDataGridView.Rows.Count - 1)].Cells[2].Value = "";
+                mainDataGridView.Rows[(mainDataGridView.Rows.Count - 1)].Cells[3].Value = "Browse";
+                mainDataGridView.Rows[(mainDataGridView.Rows.Count - 1)].Cells[4].Value = "";
+                mainDataGridView.Rows[(mainDataGridView.Rows.Count - 1)].Cells[5].Value = "Browse";
+
+            }
+
+            if (mainDataGridView.Rows.Count.ToString().Trim() == "")
+            {
+
+                mainDataGridView.Rows.Add(1);
+                mainDataGridView.Rows[(mainDataGridView.Rows.Count - 1)].Cells[0].Value = false;
+                mainDataGridView.Rows[(mainDataGridView.Rows.Count - 1)].Cells[1].Value = "";
+                mainDataGridView.Rows[(mainDataGridView.Rows.Count - 1)].Cells[2].Value = "";
+                mainDataGridView.Rows[(mainDataGridView.Rows.Count - 1)].Cells[3].Value = "Browse";
+                mainDataGridView.Rows[(mainDataGridView.Rows.Count - 1)].Cells[4].Value = "";
+                mainDataGridView.Rows[(mainDataGridView.Rows.Count - 1)].Cells[5].Value = "Browse";
+            }
+
+            fileN.Close();
+
+        }
+
+        private void mainDataGridView_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            OpenFileDialog openFileDialog1 = new OpenFileDialog();
+            if (e.ColumnIndex == 3)
+            {
+                
+                DialogResult result = openFileDialog1.ShowDialog();
+                if (result == DialogResult.OK)
+                {
+                    mainDataGridView.Rows[(mainDataGridView.Rows.Count - 1)].Cells[2].Value = openFileDialog1.FileName;  
+                }
+            }
+            if (e.ColumnIndex == 5)
+            {
+                DialogResult result = openFileDialog1.ShowDialog();
+                if (result == DialogResult.OK)
+                {
+                    mainDataGridView.Rows[(mainDataGridView.Rows.Count - 1)].Cells[4].Value = openFileDialog1.FileName;
+                }
+                
+            }
+            
+        }
+
+        private void saveButton_Click(object sender, EventArgs e)
+        {
+            var aDataSet = new DataSet();
+            
+
+
+            var anotherDataTable = new DataTable { TableName = "DataGrid" };
+            anotherDataTable.Columns.Add("check");
+            anotherDataTable.Columns.Add("rowName");
+            anotherDataTable.Columns.Add("iLocation");
+            anotherDataTable.Columns.Add("iBrowse");
+            anotherDataTable.Columns.Add("dLocation");
+            anotherDataTable.Columns.Add("dBrowse");
+            aDataSet.Tables.Add(anotherDataTable);
+
+
+            foreach (DataGridViewRow c in mainDataGridView.Rows)
+            {
+                DataRow anotherRow = aDataSet.Tables["DataGrid"].NewRow();
+                anotherRow["check"] = c.Cells[0].Value.ToString();
+                anotherRow["rowName"] = c.Cells[1].Value.ToString();
+                anotherRow["iLocation"] = c.Cells[2].Value.ToString();
+                anotherRow["iBrowse"] = c.Cells[3].Value.ToString();
+                anotherRow["dLocation"] = c.Cells[4].Value.ToString();
+                anotherRow["dBrowse"] = c.Cells[5].Value.ToString();
+                aDataSet.Tables["DataGrid"].Rows.Add(anotherRow);
+            }
+
+            SaveFileDialog(aDataSet);
+            
+        }
+
+        private void SaveFileDialog(DataSet aDataSet)
+        {
+            SaveFileDialog saveFileDialog1 = new SaveFileDialog();
+
+            saveFileDialog1.InitialDirectory = @"D:\";
+
+            saveFileDialog1.Title = "Save State Files";
+
+            //saveFileDialog1.CheckFileExists = true;
+
+            saveFileDialog1.CheckPathExists = true;
+
+            saveFileDialog1.DefaultExt = "xml";
+
+            saveFileDialog1.Filter = "xml files (*.xml)|*.xml|All files (*.*)|*.*";
+
+            saveFileDialog1.FilterIndex = 2;
+
+            saveFileDialog1.RestoreDirectory = true;
+
+
+
+            if (saveFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                aDataSet.WriteXml(saveFileDialog1.FileName);
+                MessageBox.Show("Saved in " + saveFileDialog1.FileName);
+                
+
+            }
+        }
+
+        private void LoadButton_Click(object sender, EventArgs e)
+        {
+            var newDataset = new DataSet();
+            
+
+            string openedFileName = OpenFile();
+
+            try
+            {
+                newDataset.ReadXml(openedFileName);
+
+                foreach (DataRow i in newDataset.Tables["DataGrid"].Rows)
+                {
+                    var n = mainDataGridView.Rows.Add();
+                    mainDataGridView.Rows[n].Cells[0].Value = i[0].ToString();
+                    mainDataGridView.Rows[n].Cells[1].Value = i[1].ToString();
+                    mainDataGridView.Rows[n].Cells[2].Value = i[2].ToString();
+                    mainDataGridView.Rows[n].Cells[3].Value = i[3].ToString();
+                    mainDataGridView.Rows[n].Cells[4].Value = i[4].ToString();
+                    mainDataGridView.Rows[n].Cells[5].Value = i[5].ToString();
+                }
+            }
+            catch (Exception)
+            {
+
+                
+            }
+            
+        }
+
+        private string OpenFile()
+        {
+            OpenFileDialog openFileDialog1 = new OpenFileDialog();
+
+            string fileName = "";
+                DialogResult result = openFileDialog1.ShowDialog();
+                if (result == DialogResult.OK)
+                {
+                   fileName = openFileDialog1.FileName;
+                   mainDataGridView.Rows.Clear();
+                   mainDataGridView.DataSource = null;
+                }
+
+            return fileName;
         }
     }
 }
